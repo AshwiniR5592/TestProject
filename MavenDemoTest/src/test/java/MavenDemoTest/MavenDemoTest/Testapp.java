8391b3025e112fda584f7c02bd98b7a9f9a98f99
AshwiniR5592 <ashwinirathod5592@gmail.com>
2022-01-08 15:55:35 +0530

    first commit in mavendemotest

---
+++ MavenDemoTest/src/test/java/MavenDemoTest/MavenDemoTest/Testapp.java
@@ -0,0 +1,19 @@
+package MavenDemoTest.MavenDemoTest;
+
+import org.testng.annotations.Test;
+
+public class Testapp {
+	
+	@Test
+	public void createTest()
+	{
+		System.out.println("testing completed");
+	}
+	
+	
+	
+	
+	
+}
+	
+
